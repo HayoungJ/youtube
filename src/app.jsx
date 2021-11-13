ebc2c5b6b05a5acd69a0b1f6d3c3da42fb72a69e
Hayoung Jeon <hayoungj0710@gmail.com>
2021-11-13 21:22:09 +0900

    추가 : App에서 Youtube api를 호출하도록 기능 추가
    App이 mounted 될 때 Youtube api를 호출하도록 기능 추가
    받아온 api data는 그 중 직접적인 비디오 데이터를 가지고 있는 items 부분만 잘라 popularVideoList에 저장되도록 설계
    popularVideoList는 동영상 리스트를 보여주는 Videos Component에 전달

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
+import { Component } from 'react';
 import './app.css';
+import Videos from './components/videos';
 
-function App() {
-  return <h1>Hello :&#41;</h1>;
+class App extends Component {
+  state = {
+    popularVideoList: [],
+  };
+
+  componentDidMount() {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC0TTfG7oyacZt0lha5u9O9bC01JGaMtI0',
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) =>
+        this.setState({ popularVideoList: JSON.parse(result).items })
+      )
+      .catch((error) => console.log('error', error));
+  }
+
+  render() {
+    return <Videos popularVideoList={this.state.popularVideoList} />;
+  }
 }
 
 export default App;
