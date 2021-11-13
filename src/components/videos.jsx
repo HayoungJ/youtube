fe7f5f9596dacd8269fe6cfd527c3733849e9234
Hayoung Jeon <hayoungj0710@gmail.com>
2021-11-13 23:45:44 +0900

    추가 : Video component에 동영상 선택 기능 추가
    동영상이 선택되면 id를 받아와 VideoInfo component에 넘겨주도록 기능 추가

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -1,15 +1,60 @@
 import React, { Component } from 'react';
 import Video from './video';
+import VideoInfo from './videoInfo';
 import styles from './videos.module.css';
 
 class Videos extends Component {
+  state = {
+    isVideoSelected: false,
+    selectedVideo: {},
+  };
+
+  onSelect = (id) => {
+    const selectedVideoArray = this.props.popularVideoList.filter(
+      (video) => video.id === id
+    );
+    if (selectedVideoArray.length === 1) {
+      this.setState({ isVideoSelected: true });
+      this.setState({ selectedVideo: selectedVideoArray[0] });
+    } else {
+      console.log(
+        'error : 같은 아이디를 가진 동영상이 두 개 이상일 수 없습니다.'
+      );
+    }
+  };
+
   render() {
     return (
-      <ul className={styles.list}>
-        {this.props.popularVideoList.map((video) => {
-          return <Video key={video.id} info={video.snippet} />;
-        })}
-      </ul>
+      <div
+        className={`${styles.body} ${
+          this.state.isVideoSelected
+            ? styles[`info-opened`]
+            : styles[`info-closed`]
+        }`}
+      >
+        {this.state.isVideoSelected ? (
+          <VideoInfo
+            key={this.state.selectedVideo.id}
+            id={this.state.selectedVideo.id}
+            info={this.state.selectedVideo.snippet}
+          />
+        ) : (
+          <></>
+        )}
+        <ul className={styles.list}>
+          {this.props.popularVideoList.map((video) => {
+            return (
+              <Video
+                key={video.id}
+                id={video.id}
+                info={video.snippet}
+                isVideoSelected={this.state.isVideoSelected}
+                onSelect={this.onSelect}
+              />
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
