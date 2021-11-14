1976bfdfb9d3c7f9bbe31ad368d974c4046a63c1
Hayoung Jeon <hayoungj0710@gmail.com>
2021-11-14 15:35:49 +0900

    추가 : 동영상 상세 정보의 더보기/간략히 기능 추가
    동영상 상세 정보의 더보기/간략히 기능 추가

--- src/components/videoInfo.jsx
+++ src/components/videoInfo.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import styles from './videoInfo.module.css';
 
 class VideoInfo extends Component {
+  state = {
+    isDescOpened: false,
+  };
+
+  handleClick = () => {
+    this.setState({ isDescOpened: !this.state.isDescOpened });
+  };
+
   render() {
+    let descKey = 0;
     return (
       <div className={styles.video}>
-        <div className={styles[`player-wrapper`]}>
+        <div className={styles['player-wrapper']}>
           <iframe
             className={styles.player}
             src={`http://www.youtube.com/embed/${this.props.id}`}
@@ -16,18 +25,29 @@ class VideoInfo extends Component {
         <div className={styles.info}>
           <span className={styles.title}>{this.props.info.title}</span>
           <span className={styles.channel}>{this.props.info.channelTitle}</span>
-          <div className={styles[`description-wrap`]}>
-            <span className={styles.description}>
+          <div className={styles['description-wrap']}>
+            <span
+              className={`${styles.description} ${
+                this.state.isDescOpened
+                  ? styles['full-description']
+                  : styles['short-description']
+              }`}
+            >
               {this.props.info.description.split('\n').map((line) => {
                 return (
-                  <span>
+                  <span key={Date.now() + descKey++}>
                     {line}
                     <br />
                   </span>
                 );
               })}
             </span>
-            <span className={styles[`more-description`]}>더보기</span>
+            <span
+              className={styles['more-description-btn']}
+              onClick={this.handleClick}
+            >
+              {this.state.isDescOpened ? '간략히' : '더보기'}
+            </span>
           </div>
         </div>
       </div>
