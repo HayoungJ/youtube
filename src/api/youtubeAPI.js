f53248cd8309765123e87664e2c8f0fde1a51baa
Hayoung Jeon <hayoungj0710@gmail.com>
2021-11-16 00:00:00 +0900

    추가 : Youtube API를 받아오기 위한 함수들 분리/추가
    App component 안에서 전부 관리하기에는 양이 방대해진 것 같아, 새로운 파일로 분리
    분리하는 과정에서 component에서 보내주는 데이터에 따라 유동적으로 데이터를 fetch 해 올 수 있도록, 각종 build 함수들 추가
    parseVideoData 함수가 보다 정형화된 video object를 되돌려줄 수 있도록 수정

---
+++ src/api/youtubeAPI.js
@@ -0,0 +1,104 @@
+const baseUrl = 'https://youtube.googleapis.com/youtube/v3';
+
+const buildAPIRequest = (method, path, params) => {
+  const requestOptions = {
+    method,
+    redirect: 'follow',
+  };
+
+  let url;
+  try {
+    url = buildAPIUrl(path, params);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return fetch(url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => console.log('error', error));
+};
+
+const buildAPIUrl = (path, params) => {
+  let url = `${baseUrl}${path}?`;
+  Object.entries(params).forEach(([key, value]) => {
+    if (!key || !value) return;
+    url += `&${key}=${value}`;
+  });
+  if (url) return `${url}&key=${process.env.REACT_APP_YOUTUBE_API}`;
+  else throw new Error('Failed to build request url');
+};
+
+const parseVideoData = async (result) => {
+  const data = JSON.parse(result);
+  const nextPageToken = data.nextPageToken;
+  const videoList = await Promise.all(
+    data.items
+      .map((video) => {
+        const videoId =
+          typeof video.id === 'string'
+            ? video.id
+            : typeof video.id.videoId === 'string'
+            ? video.id.videoId
+            : undefined;
+        const { title, description, thumbnails, channelTitle } = video.snippet;
+        return {
+          id: videoId,
+          title,
+          description,
+          thumbnail: thumbnails.high.url,
+          channelTitle,
+        };
+      })
+      .filter((video) => video.id)
+  );
+
+  return { nextPageToken, videoList };
+};
+
+export const fetchPopularVideo = async (
+  { publishedBefore, pageToken },
+  maxResults = 24
+) => {
+  if (!publishedBefore)
+    throw new Error(
+      `There is missing params to load popular video - publishedBefore: ${publishedBefore}`
+    );
+
+  const result = await buildAPIRequest('GET', '/videos', {
+    part: 'id, snippet',
+    chart: 'mostPopular',
+    maxResults,
+    fields:
+      'nextPageToken, items(id, snippet(title, description, thumbnails(high(url)), channelTitle))',
+    pageToken,
+    publishedBefore,
+  });
+
+  return await parseVideoData(result);
+};
+
+export const fetchSearchedVideo = async (
+  { q, publishedBefore, pageToken },
+  maxResults = 24
+) => {
+  if (!q || !publishedBefore)
+    throw new Error(
+      `There is missing params to load popular video - q: ${q}, publishedBefore: ${publishedBefore}`
+    );
+
+  const result = await buildAPIRequest('GET', '/search', {
+    part: 'id, snippet',
+    maxResults,
+    q,
+    fields:
+      'nextPageToken, items(id, snippet(title, description, thumbnails(high(url)), channelTitle))',
+    order: 'date',
+    pageToken,
+    publishedBefore,
+  });
+
+  return await parseVideoData(result);
+};
