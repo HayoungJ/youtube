d687f6eb2dbb5fb2b160c4eae30116ac705a0144
Hayoung Jeon <hayoungj0710@gmail.com>
2021-11-16 20:37:11 +0900

    리팩토링 : Content
    기존에 App 내에서 관리되던 contents 요소들을 하나의 component로 분리

---
+++ src/components/content.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import styles from './content.module.css';
+import { getRandomVideo } from '../utils';
+import Videos from './video/videos';
+import VideoPlayer from './video/videoPlayer';
+import Loader from './loader';
+
+class Content extends Component {
+  render() {
+    const {
+      videoList,
+      videoPlaying: { isVideoPlaying, videoInfo },
+      isLoading,
+      handlePlay,
+      observerRef,
+    } = this.props;
+    return (
+      <>
+        <section className={styles.content}>
+          {isVideoPlaying ? <VideoPlayer info={videoInfo} /> : <></>}
+          <Videos
+            videoList={
+              isVideoPlaying ? getRandomVideo(videoList, 6) : videoList
+            }
+            handlePlay={handlePlay}
+            width={isVideoPlaying ? '300px' : '100%'}
+          />
+          {isLoading ? <Loader /> : <></>}
+        </section>
+        <div ref={observerRef} className={styles.observer}></div>
+      </>
+    );
+  }
+}
+
+export default Content;
